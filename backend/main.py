3c2c688201b0e6eabea2e9286d9bf01e9fa8335a
manjunath9446 <manju.r.k9446@gmail.com>
2025-06-19 08:48:34 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,11 +22,12 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_methods=["*"],  # Allow all methods (POST, GET, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
+
 
 
 
